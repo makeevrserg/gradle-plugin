6188d257a56525cb21f78d6ac3053413316a7c96
makeevrserg <makeevrserg@gmail.com>
2024-06-11 21:59:44 +0300

    fix info-root plugin

--- build-logic/plugins/convention/src/main/kotlin/ru/astrainteractive/gradleplugin/InfoRootPlugin.kt
+++ build-logic/plugins/convention/src/main/kotlin/ru/astrainteractive/gradleplugin/InfoRootPlugin.kt
@@ -2,23 +2,13 @@ package ru.astrainteractive.gradleplugin
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import ru.astrainteractive.gradleplugin.property.extension.ModelPropertyValueExt.hierarchyGroup
 import ru.astrainteractive.gradleplugin.property.extension.ModelPropertyValueExt.requireProjectInfo
 
 class InfoRootPlugin : Plugin<Project> {
     override fun apply(target: Project) {
         val projectInfo = target.requireProjectInfo
-        if (target.rootProject.group.toString().isEmpty()) {
-            target.rootProject.group = projectInfo.group
-        }
-        if (target.group.toString().isEmpty()) {
-            target.group = target.hierarchyGroup
-        }
-        if (target.version.toString().isEmpty()) {
-            target.version = projectInfo.versionString
-        }
-        if (target.description.isNullOrBlank()) {
-            target.description = projectInfo.description
-        }
+        target.group = projectInfo.group
+        target.version = projectInfo.versionString
+        target.description = projectInfo.description
     }
 }

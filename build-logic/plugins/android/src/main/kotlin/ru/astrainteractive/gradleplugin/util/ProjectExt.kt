d7bfa5f7b78b078f2af41a3cec66e384591fcce5
Roman Makeev <makeevrserg@gmail.com>
2025-11-21 16:49:48 +0300

    fix sdk apply

--- build-logic/plugins/android/src/main/kotlin/ru/astrainteractive/gradleplugin/util/ProjectExt.kt
+++ build-logic/plugins/android/src/main/kotlin/ru/astrainteractive/gradleplugin/util/ProjectExt.kt
@@ -6,5 +6,5 @@ internal val Project.hasAndroidKmpPlugin: Boolean
     get() = plugins
         .hasPlugin("com.android.kotlin.multiplatform.library")
 internal val Project.hasAndroidLibPlugin: Boolean
-    get() = plugins
-        .hasPlugin("com.android.library")
+    get() = plugins.hasPlugin("com.android.library")
+        .or(plugins.hasPlugin("com.android.application"))

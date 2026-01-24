f7d1f74049b0b0739aadce99783f6bcf5394c99b
Roman Makeev <makeevrserg@gmail.com>
2026-01-24 10:06:35 +0300

    apk name update

--- build-logic/plugins/android/src/main/kotlin/ru/astrainteractive/gradleplugin/plugin/ApkNamePlugin.kt
+++ build-logic/plugins/android/src/main/kotlin/ru/astrainteractive/gradleplugin/plugin/ApkNamePlugin.kt
@@ -1,29 +1,33 @@
 package ru.astrainteractive.gradleplugin.plugin
 
+import com.android.build.api.artifact.SingleArtifact
+import com.android.build.api.variant.AndroidComponentsExtension
+import com.android.build.api.variant.ApplicationAndroidComponentsExtension
+import com.android.build.api.variant.ApplicationVariant
+import com.android.build.api.variant.impl.VariantOutputImpl
 import com.android.build.gradle.AbstractAppExtension
 import com.android.build.gradle.internal.api.BaseVariantOutputImpl
+import org.gradle.api.DefaultTask
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.file.DirectoryProperty
+import org.gradle.api.tasks.InputDirectory
+import org.gradle.api.tasks.TaskAction
 import org.gradle.kotlin.dsl.configure
+import org.gradle.kotlin.dsl.register
 import ru.astrainteractive.gradleplugin.property.extension.ModelPropertyValueExt.requireProjectInfo
+import java.io.File
 
-/**
- * This plugin will name output android apk file with custom name like below:
- *
- * ProjectName_0.0.1_debug.apk
- */
 class ApkNamePlugin : Plugin<Project> {
     override fun apply(target: Project) {
         val projectInfo = target.requireProjectInfo
-        target.configure<AbstractAppExtension> {
-            buildTypes {
-                applicationVariants.onEach { variant ->
-                    variant.outputs.onEach { output ->
-                        if (output is BaseVariantOutputImpl) {
-                            val name = projectInfo.name
-                            val version = projectInfo.versionString
-                            output.outputFileName = "${name}_${version}_${variant.name}.apk"
-                        }
+        target.configure<ApplicationAndroidComponentsExtension> {
+            onVariants { variant ->
+                variant.outputs.onEach { output ->
+                    if (output is VariantOutputImpl) {
+                        val name = projectInfo.name
+                        val version = projectInfo.versionString
+                        output.outputFileName.set("${name}_${version}_${variant.name}.apk")
                     }
                 }
             }
